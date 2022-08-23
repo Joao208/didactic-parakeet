6e324c5aa9a0fcda91ca325ad726a9d8a574b6a9
João Augusto <augustoj311@gmail.com>
2022-08-23 20:36:36 -0300

    fix: fixing ts config and adding tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,3 +1,33 @@
-test("adds 1 + 2 to equal 3", () => {
-  expect(1 + 2).toBe(3)
+import LogTime from "./index"
+
+jest.setTimeout(10000)
+
+describe("[LogTime] Test", () => {
+  it("Should call function with message and time", async () => {
+    const myConsole = {
+      info: jest.fn(),
+    }
+
+    const logTime = new LogTime(myConsole.info)
+
+    logTime.start()
+
+    await new Promise((r) => setTimeout(r, 1000))
+
+    logTime.stop("My function delayed {{seconds}} {{minutes}} {{hours}} {{days}}")
+
+    expect(myConsole.info).toHaveBeenCalledTimes(1)
+  })
+
+  it("Should return 0 in time if not started the function", async () => {
+    const myConsole = {
+      info: jest.fn(),
+    }
+
+    const logTime = new LogTime(myConsole.info)
+
+    logTime.stop("My function delayed {{foo}}")
+
+    expect(myConsole.info).toHaveBeenCalledTimes(1)
+  })
 })
